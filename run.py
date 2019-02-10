62859939ca202a07bf05125407e7ced7a62b4d7c
Harold Espinoza <haroldesptru@gmail.com>
2019-02-10 17:38:38 -0600

    modificado: 'run.py', modificado el script principal del servidor. Ruta para la API completa. Codigo abierto a correciones y mejoras en funcionabilidad.

--- run.py
+++ run.py
@@ -1,5 +1,26 @@
-from flask import Flask
+from flask import Flask, request, url_for, redirect
+from md5Encode import md5Encode
+from console import console
+from jsonResponse import jsonResponse
+from datetime import datetime
 
 app = Flask(__name__)
 
-app.run()
+@app.errorhandler(404)
+def page_not_found(err):
+    console.error("Error: {}. \nUrl: {}. Redirecting to index.".format(err, request.url))
+    return redirect(url_for('index'))
+
+@app.route('/md5encoder/api/')
+@app.route('/md5encoder/api/<data>')
+@jsonResponse
+def index(data=""):
+    JSON = {}
+    JSON['date'] = str(datetime.now())
+    JSON['data'] = md5Encode('hola') if data != "" else "Sorry! We don't receive data to encode"
+    return JSON
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
