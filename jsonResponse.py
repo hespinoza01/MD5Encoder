d1aa56b73b61e06be88e7eca5bcf97fd9a65e983
Harold Espinoza <haroldesptru@gmail.com>
2019-02-10 17:36:31 -0600

    modificado: 'jsonResponse.py', correcion de errores en el decorador de respuestas json. Se especifico el Content-Type y mimetype dentro del decorador.

--- jsonResponse.py
+++ jsonResponse.py
@@ -5,9 +5,10 @@ from json import dumps
 def jsonResponse(callback):
     @wraps(callback)
     def decorated(*args, **kwargs):
-        function = callback(*args, **kwargs)
-        JSON = dumps(function, indent=4, sort_keys=True)
+        JSON = dumps(dict(callback(*args, **kwargs)), indent=4, sort_keys=True)
         response = make_response(JSON)
-        response.headers['content-type'] = 'application/json; charset=utf-8'
+        response.headers['Content-Type'] = 'application/json; charset=utf-8'
+        response.headers['mimetype'] = 'application/json'
+        response.status_code = 200
         return response
     return decorated
